5fc7440dee1c6404bcc019d4be96f46ac61ef272
SavelyOm <savelii@jinr.ru>
2025-09-09 11:06:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setup(
         'tqdm>=4.67.1',
         'matplotlib>=3.10.0',
         'networkx>=3.4.2',
-        'torch-geometric>=2.6.1'
+        'torch-geometric>=2.6.1',
+        'scikit-learn>=1.6.0'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
